7cbc601802c57e61c0dc5070ed21b3006d0099ee
Ray <raydequito@gmail.com>
2024-06-11 21:41:43 +0100

    Recent games added

--- resources/views/user/edit.blade.php
+++ resources/views/user/edit.blade.php
@@ -9,6 +9,27 @@
             'id' => $user->id
         ]),  
         'user'  => $user
-    ])
+    ])    
+</div>
+<h2>Recent Games</h2>
+<div class="mt-2">
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">Game</th>
+                <th scope="col">Game Date</th>
+                <th scope="col">Score</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($user->games_players as $game)
+            <tr>
+                <td>{{ $game->game->title }}</td>
+                <td>{{ $game->game->created_at }}</td>
+                <td>{{ $game->score }}</td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>    
 </div>
 @endsection
